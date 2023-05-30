e59f7ee7cfd3f04d36cb361cfa660fb5e20b29f2
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-05-30 13:44:11 -0500

    solucion de Errores en el componente

--- components/Error.js
+++ components/Error.js
@@ -1,38 +1,40 @@
-import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import PropTypes from 'prop-types';
-import Colors from '../theme/Colors';
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import Colors from "../theme/Colors";
 
 const propTypes = {
-    errorText1: PropTypes.string,
-    errorText2: PropTypes.string,
-}
+  errorText1: PropTypes.string,
+  errorText2: PropTypes.string,
+};
 
 const defaultProps = {
-    errorText1: 'Ups!! algo falló',
-    errorText2: 'Verifica tu conexión a internet o reinicia el App',
-}
+  errorText1: "Ups!! algo falló",
+  errorText2: "Verifica tu conexión a internet o reinicia el App",
+};
 
-class Error extends React.PureComponent{
-    render(){
-        const {errorText1,errorText2} = this.props;
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>{errorText1}</Text>
-                <Text style={styles.text}>{errorText2}</Text>
-            </View>
-        )
-    }
+class ErrorComponent extends React.PureComponent {
+  render() {
+    const { errorText1, errorText2 } = this.props;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{errorText1}</Text>
+        <Text style={styles.text}>{errorText2}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    text: {
-      fontWeight: 'bold',
-      color: Colors.danger,
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontWeight: "bold",
+    color: Colors.danger,
+  },
+});
+
+export default ErrorComponent;
